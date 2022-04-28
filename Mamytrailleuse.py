7b0e86d4b3f18cdd57c71d3c1260b18fe97b254c
hawkwant <hawkwant@etu.univ-grenoble-alpes.fr>
2022-04-28 17:47:06 +0200

    Version fonctionnelle du Bot

--- Mamytrailleuse.py
+++ Mamytrailleuse.py
@@ -3,7 +3,22 @@ Created on 28 avr. 2022
 
 @author: Thomas
 '''
-import cogs.Muniton as CM
+import cogs.Munition as CM
+import discord.ext.commands as DC
+import json
+import os
 
 if __name__ == '__main__':
-    print(CM.Munition().quotes)
+    if os.path.exists("data/config.json"):
+        with open("data/config.json") as file:
+            data = json.load(file)
+        
+    bot = DC.Bot(
+        command_prefix=data.get("PREFIX", "$ "),
+        case_insensitive = True)
+    bot.add_cog(CM.Munition(bot))
+    
+    # bot.load_extension("cogs.Munition")
+    
+    bot.run(data.get("TOKEN"))
+    
