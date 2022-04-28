5c8f7e3e1b1f11bbf4b61dc846eefe110db21342
hawkwant <hawkwant@etu.univ-grenoble-alpes.fr>
2022-04-28 17:05:26 +0200

    Added first cog

---
+++ Mamytrailleuse.py
@@ -0,0 +1,9 @@
+'''
+Created on 28 avr. 2022
+
+@author: Thomas
+'''
+import cogs.Muniton as CM
+
+if __name__ == '__main__':
+    print(CM.Munition().quotes)
---
+++ cogs/Muniton.py
@@ -0,0 +1,22 @@
+"""
+Created on 28 avr. 2022
+
+@author: Thomas
+"""
+import discord.ext.commands as DC
+import os
+import json
+
+class Munition(DC.Cog):
+    """
+    Un Cog qui ajoute une seule commande, pour spammer de pings un utilisateur
+    """
+    def __init__(self, bot):
+        self.bot = bot
+        if os.path.exists("data/quotes.json"):
+            with open("data/quotes.json") as file:
+                self.quotes = json.load(file)
+            
+        else:
+            self.quotes = list()
+        
